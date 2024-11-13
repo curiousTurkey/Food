9c24caad9bbfeeaff1539770cc1930be36384d87
Eldhose Babu <eldhosebabu197@gmail.com>
2024-11-13 13:51:12 -0500

    Initial Commit

---
+++ Food/FoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodApp.swift
+//  Food
+//
+//  Created by Eldhose on 2024-11-13.
+//
+
+import SwiftUI
+
+@main
+struct FoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
